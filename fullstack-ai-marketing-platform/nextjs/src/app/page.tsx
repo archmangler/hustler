8900386e505e4bd6cfed0fc7f1f1fef459609954
Traiano Welcome <traiano@gmail.com>
2025-04-02 22:38:20 +0800

    basic u.i framework

---
+++ fullstack-ai-marketing-platform/nextjs/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-red-500 text-white">
+      <h1>Hello from new project</h1>
+      <Button variant="outline">Click Me</Button>
+    </div>
+  );
+}
