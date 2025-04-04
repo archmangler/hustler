41d216fa0997629792307f01d9c9deedd0cb77f7
Traiano Welcome <traiano@gmail.com>
2025-04-04 13:07:07 +0800

    fix: remove unused features

--- fullstack-ai-marketing-platform/nextjs/src/app/page.tsx
+++ fullstack-ai-marketing-platform/nextjs/src/app/page.tsx
@@ -1,11 +1,10 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="bg-red-500 text-white">
       <h1>Hello from new project</h1>
-      <Button variant="outline">Click Me</Button>
+      <Button variant="secondary">Click Me</Button>
     </div>
   );
 }
